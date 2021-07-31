2f6e58e9d6ab899bfc734a4b2d949fd40376a18b
Uğur Arıcı <m.ugurarici@gmail.com>
2021-07-31 10:58:19 +0300

    Create base models and relations

---
+++ database/migrations/2021_07_31_075259_create_post_tag_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTagTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_tag', function (Blueprint $table) {
+            $table->foreignIdFor(Post::class);
+            $table->foreignIdFor(Tag::class);
+
+            $table->unique(['post_id', 'tag_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_tag');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,6 +17,22 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * Basit bir blog sistemi istiyoruz
+ * İçinde blog yazıları (Post) bulunacak
+ * Bu yazılar bir kullanıcıya (User) ait olacak
+ * Bu yazılar bir kategoriye (Category) bağlı olacak
+ * Bu yazılar birçok etikete (Tag) sahip olabilecek
+ *
+ * Ana sayfada en sonuncudan en eskiye, sayfa başı 5 tane gözükecek şekilde listeleme olacak
+ * Bir kullanıcının adına basılarak, o kullanıcın yazılarının listelemesi olacak
+ * Kategoriye göre listeleme olacak
+ * Etikete göre listeleme olacak
+ * Aktif bir kullanıcı varsa;
+ * Kullanıcı yeni bir yazı ekleyebilecek
+ * Kendi yazılarını düzenleyebilecek ve silebilecek
+ */
 
 Auth::routes();
 
